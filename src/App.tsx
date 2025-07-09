a8fea6cc5411ec73cc25ce0bb907d25af1ed4edc
Conner Replogle <conner.replogle@proton.me>
2025-07-09 13:27:44 -0500

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Upload from './pages/Upload';
-import Library from './pages/Library';
-import Playlist from './pages/Playlist';
+import Watch from './pages/Watch';
 import './App.css';
 import ConnerTest from './pages/ConnerTest';
 
@@ -14,8 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path=":eventId/upload" element={<Upload />} />
-          <Route path=":eventId/library" element={<Library />} />
-          <Route path="playlist" element={<Playlist />} />
+          <Route path=":eventId/watch" element={<Watch />} />
           <Route path="conner" element={<ConnerTest />} />
         </Route>
       </Routes>
--- src/pages/ConnerTest.tsx
+++ src/pages/ConnerTest.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import { VideoPlayer } from '../ components/VideoPlayer';
+import { VideoPlayer } from '../components/VideoPlayer';
 
 interface Video {
   key: string;
