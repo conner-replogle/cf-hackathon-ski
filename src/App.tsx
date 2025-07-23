fc7a10622fcb25fdb9b9c9246a1c1c259f8ff106
Conner Replogle <conner@cloudflare.com>
2025-07-23 12:41:54 -0500

    Admin page + Tanstack API POC

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,37 @@ import Home from "./pages/Home";
 import Watch from "./pages/Watch";
 import "./App.css";
 
-import Admin from "./pages/Admin";
+import { Admin } from "./pages/admin/Admin";
 import SelectEventPage from "./pages/upload/SelectEvent";
 import SelectTrailAndTurnPage from "./pages/upload/SelectTrailAndTurn";
 import SelectVideoPage from "./pages/upload/SelectVideo";
 
+import {
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { queryClient } from "./services/api";
+
+
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="upload">
-            <Route index element={<SelectEventPage />} />
-            <Route path="event" element={<SelectEventPage />} />
-            <Route path="trailandturn" element={<SelectTrailAndTurnPage />} />
-            <Route path="video" element={<SelectVideoPage />} />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="upload">
+              <Route index element={<SelectEventPage />} />
+              <Route path="event" element={<SelectEventPage />} />
+              <Route path="trailandturn" element={<SelectTrailAndTurnPage />} />
+              <Route path="video" element={<SelectVideoPage />} />
+            </Route>
+            <Route path="watch/:runId" element={<Watch />} />
+            <Route path="admin" element={<Admin />} />
           </Route>
-          <Route path="watch/:runId" element={<Watch />} />
-          <Route path="admin" element={<Admin />} />
-        </Route>
       </Routes>
     </Router>
+    </QueryClientProvider>
   );
 }
 
